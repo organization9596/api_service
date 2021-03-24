111eb6123c8d8de02c9b1ed89637356c85450d56
tung <tung@tungs.local>
2021-03-24 11:49:22 +0700

    change base

--- lib/service/service_config.dart
+++ lib/service/service_config.dart
@@ -1,43 +1,47 @@
 import 'package:dio/dio.dart';
 
 class ServerConfig {
+  final String? _productBaseUrl;
+  final String? _mockBaseUrl;
+  final String? _developBaseUrl;
+  final int? _timeOut;
+  final List<Interceptor>? _interceptors;
+  Dio? _restClient;
+
+  ServerConfig(ServiceConfigBuilder builder)
+      : _productBaseUrl = builder.productBaseUrl,
+        _mockBaseUrl = builder.mockBaseUrl,
+        _developBaseUrl = builder.developBaseUrl,
+        _timeOut = builder.timeOut,
+        _interceptors = builder.interceptors;
+
+  Dio getRestClient() {
+    if (_restClient == null) {
+      _restClient = Dio()
+        ..options =
+            BaseOptions(receiveTimeout: _timeOut, connectTimeout: _timeOut)
+        ..interceptors?.addAll(_interceptors);
+    }
+    return _restClient ?? Dio();
+  }
+
+  String getProductBaseUrl() {
+    return _productBaseUrl ?? "";
+  }
+
+  String getMockBaseUrl() {
+    return _mockBaseUrl ?? "";
+  }
+
+  String getDevelopBaseUrl() {
+    return _developBaseUrl ?? "";
+  }
+}
+
+class ServiceConfigBuilder {
   String? productBaseUrl;
   String? mockBaseUrl;
   String? developBaseUrl;
-  String? url;
   int? timeOut;
-  Interceptor? interceptor;
-  Dio? restClient;
-  static ServerConfig? _instance;
-
-  static ServerConfig getInstace(
-      {String? url,
-      int? timeOut,
-      String? productBaseUrl,
-      String? developBaseUrl,
-      String? mockBaseUrl,
-      Interceptor? interceptor}) {
-    if (_instance == null) {
-      _instance = ServerConfig.inital(
-          url: url,
-          timeOut: timeOut,
-          productBaseUrl: productBaseUrl,
-          mockBaseUrl: mockBaseUrl,
-          developBaseUrl: developBaseUrl,
-          interceptor: interceptor);
-    }
-    return _instance!;
-  }
-
-  ServerConfig.inital(
-      {this.url,
-      this.timeOut,
-      this.productBaseUrl,
-      this.developBaseUrl,
-      this.mockBaseUrl,
-      this.interceptor}) {
-    restClient = Dio()
-      ..options = BaseOptions(receiveTimeout: timeOut, connectTimeout: timeOut)
-      ..interceptors?.addAll([interceptor ?? Interceptor()]);
-  }
+  List<Interceptor>? interceptors;
 }
